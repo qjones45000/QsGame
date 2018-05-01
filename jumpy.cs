dabc4f9a96c47bf6d4c790014f9ba16f12228895
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-05-01 15:33:13 -0400

    Add files via upload

---
+++ jumpy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpy : MonoBehaviour {
+
+    public float jetpackForce = 75.0f;
+    Rigidbody2D rigidbody2D;
+
+     void Start()
+    {
+          rigidbody2D.GetComponent<Rigidbody2D>();
+    }
+
+
+
+    void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("1");
+
+        if (jetpackActive)
+        {
+            rigidbody2D.AddForce(new Vector2(0, jetpackForce));
+        }
+    }
+
+
+
+}
